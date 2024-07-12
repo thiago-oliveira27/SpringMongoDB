aa7238b1d6e97b6eb7e6c8dddb4947b58224f48c
thiago <thiagoolcarv27@gmail.com>
2024-07-11 22:44:03 -0300

    creating AuthorDTO using User entity

--- src/main/java/com/spring/mongo/config/Instantiation.java
+++ src/main/java/com/spring/mongo/config/Instantiation.java
@@ -2,7 +2,6 @@ package com.spring.mongo.config;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,32 +9,36 @@ import org.springframework.context.annotation.Configuration;
 
 import com.spring.mongo.domain.Post;
 import com.spring.mongo.domain.User;
+import com.spring.mongo.dtos.AuthorDTO;
 import com.spring.mongo.repository.PostRepository;
 import com.spring.mongo.repository.UserRepository;
 
 @Configuration
-public class Instantiation implements CommandLineRunner{
+public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
+
 		User user1 = new User(null, "Thiago", "thiago@gmail.com");
 		User user2 = new User(null, "Felipe", "felipe@gmail.com");
 		User user3 = new User(null, "Ronaldo", "ronaldo@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2024"),"Partiu viagem!","Vou viajar para SP!\nAbraços!", user1);
-		Post post2 = new Post(null, sdf.parse("05/07/2024"),"Hello!","How u doing?", user2);
-		
+
 		userRepository.saveAll(Arrays.asList(user1, user2, user3));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem!", "Vou viajar para SP!\nAbraços!", new AuthorDTO(user1));
+		Post post2 = new Post(null, sdf.parse("05/07/2024"), "Hello!", "How u doing?", new AuthorDTO(user2));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
--- src/main/java/com/spring/mongo/domain/Post.java
+++ src/main/java/com/spring/mongo/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.spring.mongo.dtos.AuthorDTO;
+
 @Document
 public class Post implements Serializable{
 	
@@ -17,11 +19,11 @@ public class Post implements Serializable{
 	private Date date;
 	private String tittle;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	public Post() {}
 
-	public Post(String id, Date date, String tittle, String body, User author) {
+	public Post(String id, Date date, String tittle, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -63,11 +65,11 @@ public class Post implements Serializable{
 	}
 
 	
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
